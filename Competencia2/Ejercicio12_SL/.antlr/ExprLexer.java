10b739a9111251d2e1285074d80f5a6a0fc46a75
Shinon16 <cparamo2d@gmail.com>
2026-07-04 21:46:17 -0600

    Ejercicio12 Streamlit: Si es mayor de edad -22031149

---
+++ Competencia2/Ejercicio12_SL/.antlr/ExprLexer.java
@@ -0,0 +1,201 @@
+// Generated from d:/7A_Automatas/Competencia2/Ejercicio12_SL/Expr.g4 by ANTLR 4.13.1
+import org.antlr.v4.runtime.Lexer;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.atn.*;
+import org.antlr.v4.runtime.dfa.DFA;
+import org.antlr.v4.runtime.misc.*;
+
+@SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast", "CheckReturnValue", "this-escape"})
+public class ExprLexer extends Lexer {
+	static { RuntimeMetaData.checkVersion("4.13.1", RuntimeMetaData.VERSION); }
+
+	protected static final DFA[] _decisionToDFA;
+	protected static final PredictionContextCache _sharedContextCache =
+		new PredictionContextCache();
+	public static final int
+		PUBLIC=1, CLASS=2, STATIC=3, VOID=4, INT=5, STRING=6, IF=7, NUM=8, CADENA=9, 
+		ID=10, IGUAL=11, MAS=12, MAYOR_IGUAL=13, PUNTO=14, PUNTO_COMA=15, PAR_IZQ=16, 
+		PAR_DER=17, LLAVE_IZQ=18, LLAVE_DER=19, COR_IZQ=20, COR_DER=21, WS=22;
+	public static String[] channelNames = {
+		"DEFAULT_TOKEN_CHANNEL", "HIDDEN"
+	};
+
+	public static String[] modeNames = {
+		"DEFAULT_MODE"
+	};
+
+	private static String[] makeRuleNames() {
+		return new String[] {
+			"PUBLIC", "CLASS", "STATIC", "VOID", "INT", "STRING", "IF", "NUM", "CADENA", 
+			"ID", "IGUAL", "MAS", "MAYOR_IGUAL", "PUNTO", "PUNTO_COMA", "PAR_IZQ", 
+			"PAR_DER", "LLAVE_IZQ", "LLAVE_DER", "COR_IZQ", "COR_DER", "WS"
+		};
+	}
+	public static final String[] ruleNames = makeRuleNames();
+
+	private static String[] makeLiteralNames() {
+		return new String[] {
+			null, "'public'", "'class'", "'static'", "'void'", "'int'", "'String'", 
+			"'if'", null, null, null, "'='", "'+'", "'>='", "'.'", "';'", "'('", 
+			"')'", "'{'", "'}'", "'['", "']'"
+		};
+	}
+	private static final String[] _LITERAL_NAMES = makeLiteralNames();
+	private static String[] makeSymbolicNames() {
+		return new String[] {
+			null, "PUBLIC", "CLASS", "STATIC", "VOID", "INT", "STRING", "IF", "NUM", 
+			"CADENA", "ID", "IGUAL", "MAS", "MAYOR_IGUAL", "PUNTO", "PUNTO_COMA", 
+			"PAR_IZQ", "PAR_DER", "LLAVE_IZQ", "LLAVE_DER", "COR_IZQ", "COR_DER", 
+			"WS"
+		};
+	}
+	private static final String[] _SYMBOLIC_NAMES = makeSymbolicNames();
+	public static final Vocabulary VOCABULARY = new VocabularyImpl(_LITERAL_NAMES, _SYMBOLIC_NAMES);
+
+	/**
+	 * @deprecated Use {@link #VOCABULARY} instead.
+	 */
+	@Deprecated
+	public static final String[] tokenNames;
+	static {
+		tokenNames = new String[_SYMBOLIC_NAMES.length];
+		for (int i = 0; i < tokenNames.length; i++) {
+			tokenNames[i] = VOCABULARY.getLiteralName(i);
+			if (tokenNames[i] == null) {
+				tokenNames[i] = VOCABULARY.getSymbolicName(i);
+			}
+
+			if (tokenNames[i] == null) {
+				tokenNames[i] = "<INVALID>";
+			}
+		}
+	}
+
+	@Override
+	@Deprecated
+	public String[] getTokenNames() {
+		return tokenNames;
+	}
+
+	@Override
+
+	public Vocabulary getVocabulary() {
+		return VOCABULARY;
+	}
+
+
+	public ExprLexer(CharStream input) {
+		super(input);
+		_interp = new LexerATNSimulator(this,_ATN,_decisionToDFA,_sharedContextCache);
+	}
+
+	@Override
+	public String getGrammarFileName() { return "Expr.g4"; }
+
+	@Override
+	public String[] getRuleNames() { return ruleNames; }
+
+	@Override
+	public String getSerializedATN() { return _serializedATN; }
+
+	@Override
+	public String[] getChannelNames() { return channelNames; }
+
+	@Override
+	public String[] getModeNames() { return modeNames; }
+
+	@Override
+	public ATN getATN() { return _ATN; }
+
+	public static final String _serializedATN =
+		"\u0004\u0000\u0016\u0087\u0006\uffff\uffff\u0002\u0000\u0007\u0000\u0002"+
+		"\u0001\u0007\u0001\u0002\u0002\u0007\u0002\u0002\u0003\u0007\u0003\u0002"+
+		"\u0004\u0007\u0004\u0002\u0005\u0007\u0005\u0002\u0006\u0007\u0006\u0002"+
+		"\u0007\u0007\u0007\u0002\b\u0007\b\u0002\t\u0007\t\u0002\n\u0007\n\u0002"+
+		"\u000b\u0007\u000b\u0002\f\u0007\f\u0002\r\u0007\r\u0002\u000e\u0007\u000e"+
+		"\u0002\u000f\u0007\u000f\u0002\u0010\u0007\u0010\u0002\u0011\u0007\u0011"+
+		"\u0002\u0012\u0007\u0012\u0002\u0013\u0007\u0013\u0002\u0014\u0007\u0014"+
+		"\u0002\u0015\u0007\u0015\u0001\u0000\u0001\u0000\u0001\u0000\u0001\u0000"+
+		"\u0001\u0000\u0001\u0000\u0001\u0000\u0001\u0001\u0001\u0001\u0001\u0001"+
+		"\u0001\u0001\u0001\u0001\u0001\u0001\u0001\u0002\u0001\u0002\u0001\u0002"+
+		"\u0001\u0002\u0001\u0002\u0001\u0002\u0001\u0002\u0001\u0003\u0001\u0003"+
+		"\u0001\u0003\u0001\u0003\u0001\u0003\u0001\u0004\u0001\u0004\u0001\u0004"+
+		"\u0001\u0004\u0001\u0005\u0001\u0005\u0001\u0005\u0001\u0005\u0001\u0005"+
+		"\u0001\u0005\u0001\u0005\u0001\u0006\u0001\u0006\u0001\u0006\u0001\u0007"+
+		"\u0004\u0007V\b\u0007\u000b\u0007\f\u0007W\u0001\b\u0001\b\u0005\b\\\b"+
+		"\b\n\b\f\b_\t\b\u0001\b\u0001\b\u0001\t\u0001\t\u0005\te\b\t\n\t\f\th"+
+		"\t\t\u0001\n\u0001\n\u0001\u000b\u0001\u000b\u0001\f\u0001\f\u0001\f\u0001"+
+		"\r\u0001\r\u0001\u000e\u0001\u000e\u0001\u000f\u0001\u000f\u0001\u0010"+
+		"\u0001\u0010\u0001\u0011\u0001\u0011\u0001\u0012\u0001\u0012\u0001\u0013"+
+		"\u0001\u0013\u0001\u0014\u0001\u0014\u0001\u0015\u0004\u0015\u0082\b\u0015"+
+		"\u000b\u0015\f\u0015\u0083\u0001\u0015\u0001\u0015\u0000\u0000\u0016\u0001"+
+		"\u0001\u0003\u0002\u0005\u0003\u0007\u0004\t\u0005\u000b\u0006\r\u0007"+
+		"\u000f\b\u0011\t\u0013\n\u0015\u000b\u0017\f\u0019\r\u001b\u000e\u001d"+
+		"\u000f\u001f\u0010!\u0011#\u0012%\u0013\'\u0014)\u0015+\u0016\u0001\u0000"+
+		"\u0005\u0001\u000009\u0003\u0000\n\n\r\r\"\"\u0003\u0000AZ__az\u0004\u0000"+
+		"09AZ__az\u0003\u0000\t\n\r\r  \u008a\u0000\u0001\u0001\u0000\u0000\u0000"+
+		"\u0000\u0003\u0001\u0000\u0000\u0000\u0000\u0005\u0001\u0000\u0000\u0000"+
+		"\u0000\u0007\u0001\u0000\u0000\u0000\u0000\t\u0001\u0000\u0000\u0000\u0000"+
+		"\u000b\u0001\u0000\u0000\u0000\u0000\r\u0001\u0000\u0000\u0000\u0000\u000f"+
+		"\u0001\u0000\u0000\u0000\u0000\u0011\u0001\u0000\u0000\u0000\u0000\u0013"+
+		"\u0001\u0000\u0000\u0000\u0000\u0015\u0001\u0000\u0000\u0000\u0000\u0017"+
+		"\u0001\u0000\u0000\u0000\u0000\u0019\u0001\u0000\u0000\u0000\u0000\u001b"+
+		"\u0001\u0000\u0000\u0000\u0000\u001d\u0001\u0000\u0000\u0000\u0000\u001f"+
+		"\u0001\u0000\u0000\u0000\u0000!\u0001\u0000\u0000\u0000\u0000#\u0001\u0000"+
+		"\u0000\u0000\u0000%\u0001\u0000\u0000\u0000\u0000\'\u0001\u0000\u0000"+
+		"\u0000\u0000)\u0001\u0000\u0000\u0000\u0000+\u0001\u0000\u0000\u0000\u0001"+
+		"-\u0001\u0000\u0000\u0000\u00034\u0001\u0000\u0000\u0000\u0005:\u0001"+
+		"\u0000\u0000\u0000\u0007A\u0001\u0000\u0000\u0000\tF\u0001\u0000\u0000"+
+		"\u0000\u000bJ\u0001\u0000\u0000\u0000\rQ\u0001\u0000\u0000\u0000\u000f"+
+		"U\u0001\u0000\u0000\u0000\u0011Y\u0001\u0000\u0000\u0000\u0013b\u0001"+
+		"\u0000\u0000\u0000\u0015i\u0001\u0000\u0000\u0000\u0017k\u0001\u0000\u0000"+
+		"\u0000\u0019m\u0001\u0000\u0000\u0000\u001bp\u0001\u0000\u0000\u0000\u001d"+
+		"r\u0001\u0000\u0000\u0000\u001ft\u0001\u0000\u0000\u0000!v\u0001\u0000"+
+		"\u0000\u0000#x\u0001\u0000\u0000\u0000%z\u0001\u0000\u0000\u0000\'|\u0001"+
+		"\u0000\u0000\u0000)~\u0001\u0000\u0000\u0000+\u0081\u0001\u0000\u0000"+
+		"\u0000-.\u0005p\u0000\u0000./\u0005u\u0000\u0000/0\u0005b\u0000\u0000"+
+		"01\u0005l\u0000\u000012\u0005i\u0000\u000023\u0005c\u0000\u00003\u0002"+
+		"\u0001\u0000\u0000\u000045\u0005c\u0000\u000056\u0005l\u0000\u000067\u0005"+
+		"a\u0000\u000078\u0005s\u0000\u000089\u0005s\u0000\u00009\u0004\u0001\u0000"+
+		"\u0000\u0000:;\u0005s\u0000\u0000;<\u0005t\u0000\u0000<=\u0005a\u0000"+
+		"\u0000=>\u0005t\u0000\u0000>?\u0005i\u0000\u0000?@\u0005c\u0000\u0000"+
+		"@\u0006\u0001\u0000\u0000\u0000AB\u0005v\u0000\u0000BC\u0005o\u0000\u0000"+
+		"CD\u0005i\u0000\u0000DE\u0005d\u0000\u0000E\b\u0001\u0000\u0000\u0000"+
+		"FG\u0005i\u0000\u0000GH\u0005n\u0000\u0000HI\u0005t\u0000\u0000I\n\u0001"+
+		"\u0000\u0000\u0000JK\u0005S\u0000\u0000KL\u0005t\u0000\u0000LM\u0005r"+
+		"\u0000\u0000MN\u0005i\u0000\u0000NO\u0005n\u0000\u0000OP\u0005g\u0000"+
+		"\u0000P\f\u0001\u0000\u0000\u0000QR\u0005i\u0000\u0000RS\u0005f\u0000"+
+		"\u0000S\u000e\u0001\u0000\u0000\u0000TV\u0007\u0000\u0000\u0000UT\u0001"+
+		"\u0000\u0000\u0000VW\u0001\u0000\u0000\u0000WU\u0001\u0000\u0000\u0000"+
+		"WX\u0001\u0000\u0000\u0000X\u0010\u0001\u0000\u0000\u0000Y]\u0005\"\u0000"+
+		"\u0000Z\\\b\u0001\u0000\u0000[Z\u0001\u0000\u0000\u0000\\_\u0001\u0000"+
+		"\u0000\u0000][\u0001\u0000\u0000\u0000]^\u0001\u0000\u0000\u0000^`\u0001"+
+		"\u0000\u0000\u0000_]\u0001\u0000\u0000\u0000`a\u0005\"\u0000\u0000a\u0012"+
+		"\u0001\u0000\u0000\u0000bf\u0007\u0002\u0000\u0000ce\u0007\u0003\u0000"+
+		"\u0000dc\u0001\u0000\u0000\u0000eh\u0001\u0000\u0000\u0000fd\u0001\u0000"+
+		"\u0000\u0000fg\u0001\u0000\u0000\u0000g\u0014\u0001\u0000\u0000\u0000"+
+		"hf\u0001\u0000\u0000\u0000ij\u0005=\u0000\u0000j\u0016\u0001\u0000\u0000"+
+		"\u0000kl\u0005+\u0000\u0000l\u0018\u0001\u0000\u0000\u0000mn\u0005>\u0000"+
+		"\u0000no\u0005=\u0000\u0000o\u001a\u0001\u0000\u0000\u0000pq\u0005.\u0000"+
+		"\u0000q\u001c\u0001\u0000\u0000\u0000rs\u0005;\u0000\u0000s\u001e\u0001"+
+		"\u0000\u0000\u0000tu\u0005(\u0000\u0000u \u0001\u0000\u0000\u0000vw\u0005"+
+		")\u0000\u0000w\"\u0001\u0000\u0000\u0000xy\u0005{\u0000\u0000y$\u0001"+
+		"\u0000\u0000\u0000z{\u0005}\u0000\u0000{&\u0001\u0000\u0000\u0000|}\u0005"+
+		"[\u0000\u0000}(\u0001\u0000\u0000\u0000~\u007f\u0005]\u0000\u0000\u007f"+
+		"*\u0001\u0000\u0000\u0000\u0080\u0082\u0007\u0004\u0000\u0000\u0081\u0080"+
+		"\u0001\u0000\u0000\u0000\u0082\u0083\u0001\u0000\u0000\u0000\u0083\u0081"+
+		"\u0001\u0000\u0000\u0000\u0083\u0084\u0001\u0000\u0000\u0000\u0084\u0085"+
+		"\u0001\u0000\u0000\u0000\u0085\u0086\u0006\u0015\u0000\u0000\u0086,\u0001"+
+		"\u0000\u0000\u0000\u0005\u0000W]f\u0083\u0001\u0006\u0000\u0000";
+	public static final ATN _ATN =
+		new ATNDeserializer().deserialize(_serializedATN.toCharArray());
+	static {
+		_decisionToDFA = new DFA[_ATN.getNumberOfDecisions()];
+		for (int i = 0; i < _ATN.getNumberOfDecisions(); i++) {
+			_decisionToDFA[i] = new DFA(_ATN.getDecisionState(i), i);
+		}
+	}
+}
